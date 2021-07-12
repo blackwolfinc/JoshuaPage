9c7434511a3927d0297289a81b04ba63aaa2307b
bagasajiNugroho001 <bagas@logisfleet.com>
2021-07-12 22:15:29 +0700

    merubah struktur dan add page baru

--- src/pages/HasslePage.jsx
+++ src/pages/HasslePage.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import NavbarV1 from '../components/User/header/Navbar/Navbarv1/NavbarV1'
 import { Page2Section1 } from './Page2 Component/Page2-Section1/Page2Section1'
 import { Endpage } from "./Page1 Component/Page4/Endpage";
-import { Footer } from '../components/Footer/Footer';
+
 
 export const HasslePage = () => {
     return (
        <>
              <NavbarV1/>
             <Page2Section1/>
-            <Footer/>
+   
        </>
     )
 }
