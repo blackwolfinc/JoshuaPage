ecf71c55871f59a529f414b7c22155e93afa3793
bagasajiNugroho001 <bagas@logisfleet.com>
2021-08-24 21:35:00 +0700

    change to one sept

--- src/pages/Page1 Component/Countdoown/Countdown.jsx
+++ src/pages/Page1 Component/Countdoown/Countdown.jsx
@@ -4,7 +4,7 @@ import Clock from "../../../components/User/Clock/Clock"
 export const CommingSoon = () => {
     return (
         <div className="container">
-            <Clock deadline={"aug, 23, 2021"} />     
+            <Clock deadline={"sep, 01, 2021"} />     
         </div>
        
     )
